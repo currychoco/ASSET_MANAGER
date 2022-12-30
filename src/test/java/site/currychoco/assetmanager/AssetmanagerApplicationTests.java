380940cae3eacb08e1b7b553d8af23895a44795b
currychoco <godqhrgka0000@naver.com>
2022-12-30 16:31:42 +0900

    프로젝트 생성

---
+++ src/test/java/site/currychoco/assetmanager/AssetmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.currychoco.assetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
