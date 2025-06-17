52da96d0e0d0947ff4fc7468542630e35e92f3e1
currychoco <godqhrgka0000@naver.com>
2025-06-18 03:20:30 +0900

    test 파일 수정

--- src/test/java/site/currychoco/assetmanager/AssetManagerApplicationTests.java
+++ src/test/java/site/currychoco/assetmanager/AssetManagerApplicationTests.java
@@ -2,20 +2,9 @@ package site.currychoco.assetmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
-import site.currychoco.assetmanager.asset.domain.Asset;
 import site.currychoco.assetmanager.asset.repository.AssetRepository;
-import site.currychoco.assetmanager.employee.domain.EmployeeDto;
-
-import java.util.List;
 
 @Transactional
 @SpringBootTest
